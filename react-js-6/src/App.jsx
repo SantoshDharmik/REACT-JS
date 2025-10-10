2edf39c2293c50c9f88d22ba53f51d3e3e2a5140
SantoshDharmik <Santoshdharmik5@gmail.com>
2025-10-10 18:30:33 +0530

    create language grid

---
+++ react-js-6/src/App.jsx
@@ -0,0 +1,13 @@
+import LanguageGrid from "./components/LanguageGrid.jsx"
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+const App = () => {
+  return (
+    <LanguageGrid/>
+  )
+}
+
+export default App
