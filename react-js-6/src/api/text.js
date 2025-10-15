11038a88de478b280e3860d8c14174bb8952def2
SantoshDharmik <Santoshdharmik5@gmail.com>
2025-10-15 10:31:37 +0530

    to learn useEffect to fetching data smoothly

---
+++ react-js-6/src/api/text.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+let baseUrl = `${import.meta.env.VITE_API_URL}/test`
+
+export async function fetchData() {
+    try{
+        let result = await axios({
+            method: "GET",
+            url: `${baseUrl}/hello`
+        })
+        return result
+
+    } catch (error) {
+        throw error
+
+    }
+}
